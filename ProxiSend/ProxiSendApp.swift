b9d4a84d155f8e3d2efd42b965f746735b795c95
Jackson Currie <jackson@currie.page>
2025-02-16 12:26:36 +1300

    Initial Commit

---
+++ ProxiSend/ProxiSendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxiSendApp.swift
+//  ProxiSend
+//
+//  Created by Jackson Currie on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProxiSendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProxiSendTests/ProxiSendTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProxiSendTests.swift
+//  ProxiSendTests
+//
+//  Created by Jackson Currie on 16/02/2025.
+//
+
+import Testing
+
+struct ProxiSendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
